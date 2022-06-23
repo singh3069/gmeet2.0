c4feeb42837bb1fb60de7328dff26f7e4db013b5
Saurabh <raisaurabh265@gmail.com>
2022-06-24 01:26:46 +0530

    user images and headless ui added

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,5 +1,58 @@
+const userData = [
+  {
+    img: "https://avatars.githubusercontent.com/u/73482745?v=4",
+    name: "Saurabh",
+  },
+  {
+    img: "https://repository-images.githubusercontent.com/355841902/21311500-9881-11eb-96e0-7d6ac8d1b48e",
+    name: "Harry Potter",
+  },
+  {
+    img: "https://i.pinimg.com/originals/0c/fb/a6/0cfba6d0adc1c86ac536dc1a1ec795a7.jpg",
+    name: "Ron Weasley",
+  },
+  {
+    img: "https://i1.sndcdn.com/avatars-000293556165-63ie4j-t500x500.jpg",
+    name: "Hermione Granger",
+  },
+  {
+    img: "https://www.giantbomb.com/a/uploads/square_small/46/462814/3187899-5431433650-latest",
+    name: "Fred Weasley",
+  },
+  {
+    img: "https://i1.sndcdn.com/artworks-mJ2NYXcxL5kvUaTc-oEDzPQ-t500x500.jpg",
+    name: "Bill Weasley",
+  },
+  {
+    img: "https://pbs.twimg.com/profile_images/1298380607068745729/t65sdSut_400x400.jpg",
+    name: "Draco Malfoy",
+  },
+  {
+    img: "https://akns-images.eonline.com/eol_images/Entire_Site/2021117/rs_1200x1200-210217113937-1200-Bonnie-Wright-Harry-Potter-LT-21721-20070101_zva_a90_5332.jpg?fit=around%7C1200:1200&output-quality=90&crop=1200:1200;center,top",
+    name: "Ginny Weasley",
+  },
+];
 function User() {
-  return <div className=" w-full h-screen "></div>;
+  return (
+    <div className="w-full h-screen ">
+      <div className="flex">
+        {userData.map((e) => {
+          return (
+            <div key={e.name}>
+              <img
+                src={e.img}
+                alt={e.name}
+                width={108}
+                height={80}
+                className="rounded-full"
+              />
+              <p>{e.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default User;
--- src/components/footer/MessageAndUserInfo.tsx
+++ src/components/footer/MessageAndUserInfo.tsx
@@ -1,7 +1,55 @@
-import React from "react";
+import infoSVG from "../../IconsSVG/info.svg";
+import participantsSVG from "../../IconsSVG/allUsers.svg";
+import chatSVG from "../../IconsSVG/chat.svg";
+import activitiesSVG from "../../IconsSVG/activities.svg";
+import lockSVG from "../../IconsSVG/lock.svg";
+
+const data = [
+  {
+    img: infoSVG,
+    hoverText: "Turn ON/OFF Microphone",
+  },
+  {
+    img: participantsSVG,
+    hoverText: "Turn ON/OFF Camera",
+  },
+  {
+    img: chatSVG,
+    hoverText: "Turn on captions",
+  },
+  {
+    img: activitiesSVG,
+    hoverText: "Present now",
+  },
+  {
+    img: lockSVG,
+    hoverText: "More Options",
+  },
+];
 
 function MessageAndUserInfo() {
-  return <div>MessageAndUserInfo</div>;
+  return (
+    <div>
+      <div className="flex space-x-4">
+        {data.map((e) => {
+          return (
+            <div className="p-2 hover:bg-neutral-700 relative box-border rounded-full cursor-pointer">
+              <p className="hidden absolute hover:visible invisible top-0 text-sm text-red-800">
+                {e.hoverText}
+              </p>
+              <img
+                src={e.img}
+                width={22}
+                height={22}
+                className="cursor-pointer box-border"
+                alt={e.hoverText}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default MessageAndUserInfo;
